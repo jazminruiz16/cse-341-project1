495099f5b2b3c624be6359ff6ebed4e2121b3b71
jazminruiz16 <jazmingruiz@gmail.com>
2025-11-08 16:12:06 -0400

    terminal

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+app.use('/', require('./routes'));
 
 
 app.listen(port, () => { console.log(`Runing on port ${port}`) });
